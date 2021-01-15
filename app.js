de8062c2bc4b413ce71587d01288960649169d90
shahin <mohammadshahin237@gmail.com>
2021-01-14 20:40:54 -0800

    complete weather app

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const http = require('http');
+
+
+
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+
+
+app.get("/", function (req, res) {
+    res.render("html");
+
+});
+
+
+
+app.post("/", function (req, response) {
+
+    const cityName = req.body.cityName;
+    http.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=0ec50091bc2d3e6450cc9437d4f7987e&units=metric", function (req, res) {
+        req.on("data", (data) => {
+            const datas = JSON.parse(data);
+            const temps = datas.main.temp;
+            const description = datas.weather[0].description;
+            response.render("weather", {
+                temp: temps,
+                icon: description,
+                cityName: cityName
+            })
+
+        })
+    })
+
+})
+
+app.post("/weather", function (req, res) {
+    res.redirect("/")
+})
+
+
+
+app.listen(3000, function () {
+    console.log("server is running on port 3000")
+})
